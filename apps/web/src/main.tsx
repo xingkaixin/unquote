6c7b0db146cf2c4586d4edd2b9093646f1213c57
XingKaiXin <xingkaixin@gmail.com>
2026-08-04 12:42:23 +0800

    feat(web): Add Edge extension listing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,6 +7,8 @@ import { clearLegacySourceHash } from "./legacy-source-hash";
 
 const CHROME_WEB_STORE_URL =
   "https://chromewebstore.google.com/detail/unquote/ohcepfneflaihakpkkgmnbdgjhnmcjeg";
+const EDGE_ADDONS_URL =
+  "https://microsoftedge.microsoft.com/addons/detail/amdbhljchamjbhknbamkcemccmelegdp";
 
 initializeThemePreference();
 clearLegacySourceHash(window.location, window.history);
@@ -14,7 +16,7 @@ clearLegacySourceHash(window.location, window.history);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider>
-      <UnquoteApp chromeWebStoreUrl={CHROME_WEB_STORE_URL} />
+      <UnquoteApp chromeWebStoreUrl={CHROME_WEB_STORE_URL} edgeAddonsUrl={EDGE_ADDONS_URL} />
     </I18nProvider>
   </React.StrictMode>,
 );
